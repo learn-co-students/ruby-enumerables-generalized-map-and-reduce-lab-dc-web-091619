4fbb021b848f0b6f57ec27b2f72898b33abcc800
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-23 21:15:43 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    x = source_array[i]
+    new_array << yield(x)
+    i += 1
+  end
+  new_array
+end
+
+def reduce(source_array, starting = 0)
+  i = 0
+  run_total = starting
+  while i < source_array.length do
+    n = source_array[i]
+    run_total = yield(run_total, n)
+    i += 1
+  end
+  if run_total == 0
+    return TRUE
+  end
+  run_total
+end
+  
+  
+
+  
